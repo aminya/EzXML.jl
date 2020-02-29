1c77ef261324f5a910f7e1e8837b5ad9a45de330
aminya <aminyahyaabadi74@gmail.com>
2020-02-28 20:22:00 -0600

    Simpler show

--- test/runtests.jl
+++ test/runtests.jl
@@ -1301,10 +1301,10 @@ end
 @testset "Misc" begin
     @testset "show" begin
         doc = parsexml("""<root attr="val"/>""")
-        @test occursin(r"^EzXML.Node\(<DOCUMENT_NODE>\)$", repr(doc.node))
-        @test occursin(r"^EzXML.Node\(<ELEMENT_NODE\[root\]>\)$", repr(root(doc)))
-        @test occursin(r"^EzXML.Node\(<ATTRIBUTE_NODE\[attr\]>\)$", repr(attributes(root(doc))[1]))
-        @test occursin(r"^EzXML.Document\(EzXML.Node\(<DOCUMENT_NODE>\)\)$", repr(doc))
+        @test occursin(r"^Node\(<DOCUMENT_NODE>\)$", repr(doc.node))
+        @test occursin(r"^Node\(<ELEMENT_NODE\[root\]>\)$", repr(root(doc)))
+        @test occursin(r"^Node\(<ATTRIBUTE_NODE\[attr\]>\)$", repr(attributes(root(doc))[1]))
+        @test occursin(r"^EzXMLp.Document\(Node\(<DOCUMENT_NODE>\)\)$", repr(doc))
 
         sample2 = joinpath(dirname(@__FILE__), "sample2.xml")
         reader = open(EzXML.StreamReader, sample2)
